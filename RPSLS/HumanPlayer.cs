cc86403e482633c7ab5c7be323bd52f4598726e6
LizzieDworsky <amydworsky15@gmail.com>
2023-04-06 17:44:46 -0600

    added child HumanPlayer class and started logic for abstract method ChooseGesture, WIP

---
+++ RPSLS/HumanPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class HumanPlayer : Player
+    {
+        public HumanPlayer(string name) : base(name)
+        {
+        }
+
+        public override void ChooseGesture()
+        {
+            List<string> inputOptions = new List<string>
+            {
+                "0",
+                "1",
+                "2",
+                "3",
+                "4"
+            };
+            Console.WriteLine(@"
+        What gesture would you like to select
+
+            Please enter:
+            0 for Rock
+            1 for Paper
+            2 for Scissors
+            3 for Lizard
+            4 for Spock
+
+            ");
+            string input = Console.ReadLine();
+            while (!inputOptions.Contains(input))
+            {
+                Console.WriteLine($"Sorry {input} is not a valid entry. Please try again.");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
