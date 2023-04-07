ecfa3573defd4b0fd0b916b50932eb9fcd6d77c8
LizzieDworsky <amydworsky15@gmail.com>
2023-04-06 20:43:03 -0600

    created ComputerPlayer class

---
+++ RPSLS/ComputerPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class ComputerPlayer : Player
+    {
+        public ComputerPlayer() : base("Computer")
+        {
+        }
+
+        public override void ChooseGesture()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
